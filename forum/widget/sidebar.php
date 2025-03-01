9c87fcdd42bf4d49b43d1b8c5fb6548d387989d4
Mehmet Nazım Çakar <75137064+menaca@users.noreply.github.com>
2025-03-02 02:29:08 +0300

    Add files via upload

---
+++ forum/widget/sidebar.php
@@ -0,0 +1,36 @@
+<div class="inner-sidebar">
+            <div class="inner-sidebar-header justify-content-center">
+                <a class="btn btn-primary has-icon btn-block"href="create_thread.php">Konu Aç</a>
+            </div>
+
+            <div class="inner-sidebar-body p-0">
+                <div class="p-3" data-simplebar="init">
+                    <div class="simplebar-wrapper" style="margin: -16px;">
+                        <div class="simplebar-mask">
+                            <div class="simplebar-offset" style="right: 0px; bottom: 0px;">
+                                    <div class="simplebar-content" style="padding: 16px;">
+                                        <nav class="nav nav-pills nav-gap-y-1 flex-column">
+                                            <a href="index.php" class="nav-link nav-link-faded has-icon active">Tüm Konular</a>
+                                            <a href="/index.php" class="nav-link nav-link-faded has-icon">Ana Sayfa</a>
+                                            <a href="/blogs.php" class="nav-link nav-link-faded has-icon">Blog</a>
+                                            <a href="/index.php#ranks" class="nav-link nav-link-faded has-icon">Sıralama</a>
+                                        </nav>
+                                </div>
+                                </div>
+                        </div>
+                    </div>
+                    <div class="simplebar-track simplebar-horizontal" style="visibility: hidden;"><div class="simplebar-scrollbar" style="width: 0px; display: none;"></div></div>
+                    <div class="simplebar-track simplebar-vertical" style="visibility: visible;"><div class="simplebar-scrollbar" style="height: 151px; display: block; transform: translate3d(0px, 0px, 0px);"></div></div>
+                </div>
+            </div>
+        </div>
+
+
+        
+
+        <div class="inner-main">
+            <div class="d-flex justify-content-between inner-main-header">
+                <p>EkoForum'a Hoşgeldiniz. Hemen Konu Aç, Tartışalım!</p>
+                <a href="create_thread.php"  class="btn btn-primary toggle-sidebar-btn"><i class="fa fa-plus "></i></a>
+            </div>
+
